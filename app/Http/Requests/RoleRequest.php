7d2fd2ec7a8631eba73ae9dc0d3bbffb63af60b5
Byan Nur Cahyo <byannurcahyo9@gmail.com>
2025-02-05 11:34:31 +0700

    feat(quiz):form request validation role

---
+++ app/Http/Requests/RoleRequest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+
+class RoleRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public $validator;
+
+    public function failedValidation(Validator $validator)
+    {
+        $this->validator = $validator;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        if ($this->isMethod('post')) {
+            return $this->createRules();
+        }
+        return $this->updateRules();
+    }
+
+    private function createRules(): array
+    {
+        return [
+            'name' => 'required|string|max:50',
+            'access' => 'required|array',
+        ];
+    }
+
+    private function updateRules(): array
+    {
+        return [
+            'name' => 'string|max:50',
+            'access' => 'array',
+        ];
+    }
+}
