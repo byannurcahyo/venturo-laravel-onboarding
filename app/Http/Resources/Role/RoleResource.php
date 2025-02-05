f5317c16f4dfb9f22bf5d41476f2e10684fdf6ca
Byan Nur Cahyo <byannurcahyo9@gmail.com>
2025-02-05 11:34:48 +0700

    feat(quiz):resource role

---
+++ app/Http/Resources/Role/RoleResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources\Role;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class RoleResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'access' => $this->access,
+            'updated_security' => $this->updated_security,
+        ];
+    }
+}
