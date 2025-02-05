6b19e7beb512fe691e0e656248c048be5e8fa44d
Byan Nur Cahyo <byannurcahyo9@gmail.com>
2025-02-05 17:28:15 +0700

    feat(challenge): resource customer

---
+++ app/Http/Resources/Resource/CustomerResource.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Resources\Resource;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CustomerResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'phone' => $this->phone,
+            'photo_url' => $this->photo ? url('storage/'.$this->photo) : null,
+            'address' => $this->address,
+            'email' => $this->user->email,
+            // 'list' => $this->collection($request),
+            // 'meta' => [
+            //     'links' => $this->getUrlRange(1, $this->lastPage()),
+            //     'total' => $this->total(),
+            // ]
+        ];
+    }
+}
