bb0e929303e3c46bcf4f53f896092586df0a917a
Byan Nur Cahyo <byannurcahyo9@gmail.com>
2025-02-06 17:16:31 +0700

    feat(challenge): controller menu

---
+++ app/Http/Controllers/Api/ProductCategoryController.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Helpers\Product\ProductCategoryHelper;
+use App\Http\Requests\Product\CategoryRequest;
+use App\Http\Resources\Product\CategoryResource;
+
+class ProductCategoryController extends Controller
+{
+    private $category;
+    public function __construct()
+    {
+        $this->category = new ProductCategoryHelper();
+    }
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        $filter = [
+            'name' => $request->name ?? '',
+        ];
+        $categories = $this->category->getAll($filter, 25, $request->sort ?? '');
+
+        return response()->json([
+            'success' => true,
+            'data' => CategoryResource::collection($categories['data']),
+            'meta' => [
+                'total' => $categories['total']
+            ]
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(CategoryRequest $request)
+    {
+        if(isset($request->validator) && $request->validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'message' => $request->validator->errors()
+            ], 400);
+        }
+        $payload = $request->only(['name']);
+        $category = $this->category->create($payload);
+
+        if (!$category['status']) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Failed to create category'
+            ], 400);
+        }
+        return response()->json([
+            'success' => true,
+            'data' => new CategoryResource($category['data'])
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $category = $this->category->getById($id);
+
+        if (!$category['status']) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Category not found'
+            ], 404);
+        }
+        return response()->json([
+            'success' => true,
+            'data' => new CategoryResource($category['data'])
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(CategoryRequest $request, string $id)
+    {
+        if(isset($request->validator) && $request->validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'message' => $request->validator->errors()
+            ], 400);
+        }
+        $payload = $request->only(['name']);
+        $category = $this->category->update($payload, $id);
+
+        if (!$category['status']) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Failed to update category'
+            ], 500);
+        }
+        return response()->json([
+            'success' => true,
+            'data' => new CategoryResource($category['data'])
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $category = $this->category->delete($id);
+
+        if (!$category) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Failed to delete category'
+            ], 500);
+        }
+        return response()->json([
+            'success' => true,
+            'meesage' => 'Category deleted successfully'
+        ]);
+    }
+}
---
+++ app/Http/Controllers/Api/ProductController.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Helpers\Product\ProductHelper;
+use App\Http\Requests\Product\ProductRequest;
+use App\Http\Resources\Product\ProductResource;
+
+class ProductController extends Controller
+{
+    private $product;
+    public function __construct()
+    {
+        $this->product = new ProductHelper();
+    }
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $filter = [
+            'name' => request()->name ?? '',
+            'm_product_category_id' => request()->m_product_category_id ?? '',
+            'is_available' => request()->is_available ?? '',
+        ];
+        $products = $this->product->getAll($filter, 25, request()->sort ?? '');
+
+        return response()->json([
+            'success' => true,
+            'data' => ProductResource::collection($products['data']),
+            'meta' => [
+                'total' => $products['total']
+            ]
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(ProductRequest $request)
+    {
+        if (isset($request->validator) && $request->validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'message' => $request->validator->errors()
+            ], 400);
+        }
+        $payload = $request->only(['name', 'price', 'product_category_id', 'is_available', 'description', 'photo', 'details']);
+        $payload['m_product_category_id'] = $payload['product_category_id'];
+        $product = $this->product->create($payload);
+
+        if (!$product['status']) {
+            return response()->json([
+                'success' => false,
+                'message' => $product['message'] ?? 'Failed to create product'
+            ], 400);
+        }
+        return response()->json([
+            'success' => true,
+            'data' => new ProductResource($product['data'])
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $product = $this->product->getById($id);
+
+        if (!$product['status']) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Product not found'
+            ], 400);
+        }
+        return response()->json([
+            'success' => true,
+            'data' => new ProductResource($product['data'])
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(ProductRequest $request, string $id)
+    {
+        if(isset($request->validator) && $request->validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'message' => $request->validator->errors()
+            ], 400);
+        }
+        $payload = $request->only(['name', 'price', 'product_category_id', 'is_available', 'description', 'photo', 'details', 'details_deleted']);
+        $payload['m_product_category_id'] = $payload['product_category_id'];
+        unset($payload['product_category_id']);
+        $product = $this->product->update($payload, $id);
+
+        if (!$product['status']) {
+            return response()->json([
+                'success' => false,
+                'message' => $product['message'] ?? 'Failed to update product'
+            ], 400);
+        }
+        return response()->json([
+            'success' => true,
+            'data' => new ProductResource($product['data'])
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $product = $this->product->delete($id);
+
+        if (!$product) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Failed to delete category'
+            ], 400);
+        }
+        return response()->json([
+            'success' => true,
+            'meesage' => 'Category deleted successfully'
+        ]);
+    }
+}
