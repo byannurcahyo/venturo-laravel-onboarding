3a2502e6b98500a7aab53f7b2f99d7181be5bfac
Byan Nur Cahyo <byannurcahyo9@gmail.com>
2025-02-06 17:16:00 +0700

    feat(challenge): resource menu

---
+++ app/Http/Resources/Product/ProductResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources\Product;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProductResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'price' => $this->price,
+            'product_category_id' => $this->product_category_id,
+            'product_cateory_name' => isset($this->category) ? $this->category->name : "",
+            'is_available' => $this->is_available,
+            'description' => $this->description,
+            'photo_url' => $this->photo ? url('storage/'.$this->photo) : null,
+            'details' => ProductDetailResource::collection($this->details),
+        ];
+    }
+}
