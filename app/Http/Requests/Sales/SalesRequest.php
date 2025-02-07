5ff4d0a6608b504636b137865dad2daa3262e307
Byan Nur Cahyo <byannurcahyo9@gmail.com>
2025-02-07 15:11:55 +0700

    feat(quiz):form validation request sales

---
+++ app/Http/Requests/Sales/SalesRequest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Requests\Sales;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+
+class SalesRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public $validator;
+    public function failedValidation(Validator $validator)
+    {
+        $this->validator = $validator;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        if ($this->isMethod('post')) {
+            return $this->createRules();
+        }
+        return $this->updateRules();
+    }
+
+    private function createRules(): array
+    {
+        return [
+            'm_customer_id' => 'required|uuid|exists:m_customers,id',
+            'date' => 'nullable|date',
+            'product_detail' => 'required|array|min:1',
+            'product_detail.*.m_product_id' => 'required|uuid|exists:m_product,id',
+            'product_detail.*.m_product_detail_id' => 'required|uuid|exists:m_product_detail,id',
+            'product_detail.*.total_item' => 'required|integer|min:1',
+            'product_detail.*.price' => 'required|numeric|min:0',
+        ];
+    }
+
+    private function updateRules(): array
+    {
+        return [
+            'm_customer_id' => 'required|uuid|exists:m_customers,id',
+            'date' => 'nullable|date',
+            'product_detail' => 'required|array|min:1',
+            'product_detail.*.m_product_id' => 'required|uuid|exists:m_product,id',
+            'product_detail.*.m_product_detail_id' => 'required|uuid|exists:m_product_detail,id',
+            'product_detail.*.total_item' => 'required|integer|min:1',
+            'product_detail.*.price' => 'required|numeric|min:0'
+        ];
+    }
+}
